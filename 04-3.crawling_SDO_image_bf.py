fbb7d507a256072e75d9ca673bfc11f3bdcc9985
Kiehyun Kevin Park <guitar79@naver.com>
2018-10-09 10:11:26 +0900

    s

--- 04-3.crawling_SDO_image_bf.py
+++ 04-3.crawling_SDO_image_bf.py
@@ -1,11 +1,10 @@
-# Based on Python 3.5.2 | Anaconda 4.1.1
 # Print software information
-#print('Source : https://github.com/seungwonpark/SunSpotTracker')
+# print('Source : https://github.com/seungwonpark/SunSpotTracker')
 # Based on python 2.7.12 by Seungwon Park
 # change to python 3.6 by guitar79@gs.hs.kr
+
 # get data from https://sdo.gsfc.nasa.gov/assets/img/browse/
 # file name structure : 20170228_231038_1024_MHII.jpg
-
 # conda install beautifulsoup
 
 site = 'https://sdo.gsfc.nasa.gov/assets/img/browse/'
@@ -17,14 +16,19 @@ import urllib.request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+# downloading perid and time gap
 startdate = '20180101' #start date
 enddate = '20180105' #end date
-req_hour = [0, 3, 6, 9, 12, 15, 18, 21]
+time_gap = 3 #time gap
+request_hour = range(0,24,time_gap)
+#request_hour = [0, 3, 6, 9, 12, 15, 18, 21] #request hour
 
-drout = target[:-4] + '/'
-if not os.path.exists(drout):
-	os.makedirs(drout)
+#make directory for saving files
+save_folder = startdate + '-' + enddate + '-' + target[:-4] + '/'
+if not os.path.exists(save_folder):
+	os.makedirs(save_folder)
 
+#for calculating date
 start_date = datetime.date(datetime.strptime(startdate, '%Y%m%d'))
 end_date = datetime.date(datetime.strptime(enddate, '%Y%m%d'))
 duration = (end_date - start_date).days
@@ -32,13 +36,13 @@ duration = (end_date - start_date).days
 def filename_to_hour(filename):
     fileinfo = filename.split('_')
     return datetime.strptime(fileinfo[0]+fileinfo[1], '%Y%m%d%H%M%S')
-#print(filename_to_time('20170228_231038_1024_MHII.jpg'))
 
-dn_file_time = datetime.today()
+#for downloading 1 file per request_hour
+download_file_time = datetime.today()
 
 for i in range(duration):
-    dn_date = start_date + timedelta(i)
-    directory = dn_date.strftime('%Y') + '/' + dn_date.strftime('%m') + '/' + dn_date.strftime('%d') + '/'
+    download_date = start_date + timedelta(i)
+    directory = download_date.strftime('%Y') + '/' + download_date.strftime('%m') + '/' + download_date.strftime('%d') + '/'
     url = site + directory
     print ('*'*80)
     print ('trying %s ' % url)
@@ -49,16 +53,16 @@ for i in range(duration):
         filename = file_list[i].text
         #print ('debug', filename)
         if (filename[(-len(target)):] == target) \
-        and int(filename_to_hour(filename).strftime('%H')) in(req_hour) \
-        and dn_file_time.strftime('%Y%m%d%H') != filename_to_hour(filename).strftime('%Y%m%d%H') : # hour gap between downloading images
+            and int(filename_to_hour(filename).strftime('%H')) in(request_hour) \
+            and download_file_time.strftime('%Y%m%d%H') != filename_to_hour(filename).strftime('%Y%m%d%H') : 
             print ('Trying %s' % filename)
-            if os.path.exists('%s/%s' % (drout, filename)):
+            if os.path.exists('%s/%s' % (save_folder, filename)):
                 print ('*'*40)
                 print (filename + 'is exist')
             else :
-                urllib.request.urlretrieve(url+filename, '%s/%s' % (drout, filename))
+                urllib.request.urlretrieve(url+filename, '%s/%s' % (save_folder, filename))
                 print ('*'*60)
                 print ('Downloading' + filename)
-                dn_file_time = filename_to_hour(filename)
+                download_file_time = filename_to_hour(filename)
         else:
             print ('Skipping ' + filename)
--- 07-3.Draw_HR_Diagram_using_HST_data.py
+++ 07-3.Draw_HR_Diagram_using_HST_data.py
@@ -1,3 +1,7 @@
+# Print software information
+# based on python 3.6 by guitar79@gs.hs.kr
+# conda install beautifulsoup
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -20,41 +24,27 @@ for i in range(0, len(F450)):
             color[i] = F450[i,5] - F814[j,5]
             break # Once the matched item is found from F814, go to next star of F450.
 
-# Plot HR Diagram (Color Index - Instrument Magnitude)
-# axis lable, scale, etc....
+# Plot HR Diagram (Color Index - Magnitude)
+fig_size = plt.rcParams["figure.figsize"] # Get current size
 
-
-# Get current size
-fig_size = plt.rcParams["figure.figsize"]
- 
-# Prints: [8.0, 6.0]
-print ("Current size:", fig_size)
- 
 # Set figure width to 12 and height to 9
 fig_size[0] = 12
 fig_size[1] = 9
 plt.rcParams["figure.figsize"] = fig_size
-
-
-plt.plot(color[mask], F450[mask,5], 'o', ms=1, alpha=0.2)
+#plot chart
+plt.plot(color[mask], F450[mask,5], 'o', ms=2, alpha=0.2)
 plt.title('H-R Diagram of M80', fontsize=20)
 plt.xlabel('Color Index (F450W - F814W)', fontsize=18)
-plt.ylabel('Instrument Magnitude (F450W)', fontsize=18)
+plt.ylabel('Magnitude (F450W)', fontsize=18)
 plt.gca().invert_yaxis()
-
-plt.set_xlim(-0.5, 2.5)
-plt.set_ylim(15, -15)
 plt.grid()
+plt.xticks(range(-2, 6), size=14)
+plt.yticks(range(12, 30 ,2), size=14)
 
-
-#show the Diagram
+#show the chart
 plt.show()
-
-plt.savefig('H-R Diagram (Hipparcos catalog).png', dpi=None, facecolor='w', edgecolor='w',
+#save as the chart
+plt.savefig('H-R Diagram.png', dpi=None, facecolor='w', edgecolor='w',
         orientation='portrait', papertype=None, format=None,
         transparent=False, bbox_inches=None, pad_inches=0.1,
         frameon=None, metadata=None)
-
-#save the Diagram
-
-
