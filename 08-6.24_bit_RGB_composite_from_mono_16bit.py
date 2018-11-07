fa4a24090e99f24993e0729da7372067d441dbc0
Kiehyun Kevin Park <guitar79@naver.com>
2018-11-07 21:07:24 +0900

    update

--- 08-6.24_bit_RGB_composite_from_mono_16bit.py
+++ 08-6.24_bit_RGB_composite_from_mono_16bit.py
@@ -2,36 +2,38 @@
 """
 Created on Thu Oct 18 15:22:02 2018
 @author: park
-Use only 16bit monochrome fits file
+Use only 16bit monochrome png files
 """
 
 from PIL import Image
 import matplotlib.pyplot as plt
 
-# RGB 각 채널에 해당하는 파일명 입력
-R_file = 'mean_image_R.png'
-G_file = 'mean_image_G.png'
-B_file = 'mean_image_B.png'
-
-# RGB 각 채널별 파일을 8bit grayscale 이미지로 변경
-table = [ i/256 for i in range(65536)]
-
 # read each channel file
-red = Image.open(R_file)
-green = Image.open(G_file)
-blue= Image.open(B_file)
+red = Image.open('mean_image_R.png')
+green = Image.open('mean_image_G.png')
+blue= Image.open('mean_image_B.png')
+print('red :', red)
+print('green :', green)
+print('blue :', blue)
+
+# making list for converting 16bit to 8bit
+table = [ i/256 for i in range(65536)]
 
 # convert 16 bit image to 8 bit 
 red = red.point(table, 'L')
 green = green.point(table, 'L')
 blue = blue.point(table, 'L')
+print('red :', red)
+print('green :', green)
+print('blue :', blue)
 
 # composite 24bit image using 8bit
 RGB_image = Image.merge("RGB", (red, green, blue))
+print('RGB_image :', RGB_image)
 
 # display images
 plt.imshow(RGB_image, interpolation = 'None')
 plt.show()
 
 # save images
-RGB_image.save('img-out.png','png')
+RGB_image.save('RGB_24bit.png','png')
--- 08-6.48_bit_RGB_composite_from_mono_16bit.py
+++ 08-6.48_bit_RGB_composite_from_mono_16bit.py
@@ -6,15 +6,12 @@ Use only 16bit monochrome fits file
 
 ModuleNotFoundError: No module named 'cv2' 
 conda install opencv
-
-conda install img_scale
 """
 
 import numpy as np
 import cv2
 from astropy.io import fits
 import matplotlib.pyplot as plt
-
 
 # read 16bit monochrome fits file
 hdu_R = fits.open('mean_image_R.fit')
@@ -27,23 +24,29 @@ hdu_B = fits.open('mean_image_B.fit')
 data_B = hdu_B[0].data
 image_B = np.array(data_B/65536.0, dtype=np.float32)
 
+print('image_R :', image_R)
+print('image_G :', image_G)
+print('image_B :', image_B)
+
 # make empty array for RGB image
 RGB = np.zeros((image_R.shape[0], image_R.shape[1], 3), dtype=np.float32)
-
+# insert each channel image
 RGB[:,:,0] = image_B
 RGB[:,:,1] = image_G
 RGB[:,:,2] = image_R
+print('RGB :', RGB)
 
-
+# display image
 plt.imshow(RGB, interpolation = 'None', aspect='equal')
 plt.show()
 
-# write 16bit monochrome 
+# convert 16bit each channel
 RGB = np.asarray(RGB*65536.0, dtype=np.uint16)
+print('RGB :', RGB)
 
 # write 48 bit RGB png file
-cv2.imwrite('RGB.png', RGB)
+cv2.imwrite('RGB-48bit.png', RGB)
 
 # write 48bit RGB fits file
 hdu_R[0].data = RGB
-hdu_R.writeto('RGB.fit', overwrite =True)
+hdu_R.writeto('RGB-bit.fit', overwrite =True)
